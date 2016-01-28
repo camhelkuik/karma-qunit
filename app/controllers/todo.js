69713674de62a8ac883aa3dce72470dc7d4aa101
Cameron Helkuik <cameronhelkuik@gmail.com>
2016-01-28 10:41:03 -0600

    Starting from scratch......components arg

--- app/controllers/todo.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-
-export default Ember.ObjectController.extend({
-  isCompleted: function(key, value) {
-    return this.get('model').get('isCompleted');
-  }.property('model.isCompleted')
-});
