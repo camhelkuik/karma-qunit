a9c72e39c9a2d7850379751407ec55a80166fd03
Cameron Helkuik <cameronhelkuik@gmail.com>
2016-01-28 13:20:33 -0600

    Problem is with the component files, why edit and remove are not working

--- app/components/edit-todo.js
+++ app/components/edit-todo.js
@@ -3,9 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     submit(){
-      const text = this.get('text');
-      this.removeObject(text);
-      this.set(text);
+      
     }
   }
 });
